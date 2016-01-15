d2ee4623404f7672faf1e5c6ac958e9cd8f80a44
John Webb <vapesk@gmail.com>
2016-01-15 01:56:20 -0500

    Updated for 0.6.0

--- app.js
+++ app.js
@@ -19,14 +19,10 @@ SystemPackage.register(function(app, auth, database, circles) {
   //We enable routing. By default the Package Object is passed to the routes
   SystemPackage.routes(app, auth, database);
 
-  SystemPackage.aggregateAsset('css', 'common.css');
-  SystemPackage.angularDependencies(['mean-factory-interceptor']);
+  SystemPackage.angularDependencies(['ui.router', 'mean-factory-interceptor']);
   
 
   // The middleware in config/express will run before this code
-
-  // Set views path, template engine and default layout
-  app.set('views', __dirname + '/server/views');
 
   // Setting the favicon and static folder
   if(config.favicon) {
@@ -38,14 +34,8 @@ SystemPackage.register(function(app, auth, database, circles) {
   // Adding robots and humans txt
   app.useStatic(__dirname + '/public/assets/static');
 
-  SystemPackage.menus.add({
-    title: 'Log Out',
-    link: 'Log Out',
-    roles: ['authenticated'],
-    menu: 'account'
-  });
-  
+  circles.registerCircle('authenticated');
+  circles.registerCircle('anonymous');
 
   return SystemPackage;
-
 });
