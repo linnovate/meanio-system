aab540baee50c263d4a4d693e3dd15f827cc8421
drumfreak <webfreakeric@gmail.com>
2016-06-09 13:11:16 -0400

    Detecting HTTP status 401 from backend auth.requiresLogin and setting the cookie for post login redirect.

--- public/services/interceptor.js
+++ public/services/interceptor.js
@@ -1,12 +1,13 @@
 'use strict';
 
 angular.module('mean-factory-interceptor', [])
-  .factory('httpInterceptor', ['$q', '$location',
-    function($q, $location) {
+  .factory('httpInterceptor', ['$q', '$location', '$meanConfig', '$cookies',
+    function($q, $location, $meanConfig, $cookies) {
       return {
         'response': function(response) {
+
           if (response.status === 401) {
-            $location.path('/auth/login');
+            $location.url($meanConfig.loginPage);
             return $q.reject(response);
           }
           return response || $q.when(response);
@@ -15,7 +16,10 @@ angular.module('mean-factory-interceptor', [])
         'responseError': function(rejection) {
 
           if (rejection.status === 401) {
-            $location.url('/auth/login');
+
+            // This is to set the cookie so that we can redirect back to the proper urL
+            $cookies.put('redirect', $location.path());
+            $location.url($meanConfig.loginPage);
             return $q.reject(rejection);
           }
           return $q.reject(rejection);
@@ -24,9 +28,9 @@ angular.module('mean-factory-interceptor', [])
       };
     }
   ])
-//Http Interceptor to check auth failures for XHR requests
-.config(['$httpProvider',
-  function($httpProvider) {
-    $httpProvider.interceptors.push('httpInterceptor');
-  }
-]);
+  //Http Interceptor to check auth failures for XHR requests
+  .config(['$httpProvider',
+    function($httpProvider) {
+      $httpProvider.interceptors.push('httpInterceptor');
+    }
+  ]);
