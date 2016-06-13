0175ba37899bc3592d3f57a190126af5a7a38ad9
drumfreak <webfreakeric@gmail.com>
2016-06-13 15:33:24 -0400

    Added platformSettings into $rootScope at an early stage in plaform loading. You can setup /api/platformSettings as a GET route and deliver your own JSON however you like.

---
+++ public/services/platform-settings.js
@@ -0,0 +1,30 @@
+angular.module('mean.platformsettings').factory('PlatformSetting', ['$rootScope',
+  function($rootScope) {
+
+      function getPlatformSettings() {
+        this.config = {};
+
+        var self = this;
+
+        var xmlhttp = new XMLHttpRequest();
+
+        xmlhttp.onreadystatechange = function() {
+          if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
+            self.platformSettings = JSON.parse(xmlhttp.responseText);
+            $rootScope.platformSettings = self.platformSettings;
+          }
+        }
+
+        xmlhttp.open("GET", "/api/platformsettings", false);
+        xmlhttp.send();
+
+        this.$get = function() {
+          $rootScope.platformSettings = this.platformSettings;
+          return this.platformSettings;
+        };
+
+    }
+
+    return new getPlatformSettings();
+  }
+]);
