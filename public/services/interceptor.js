b8d774b6fcf3a623dbdccac59e29bccc7882359e
Tim Elfelt <timelf123@gmail.com>
2016-08-31 11:41:03 -0500

    Exception handler for error logging (#4)
    * feat(exceptions) extend angular exception handler to log all frontend errors

--- public/services/interceptor.js
+++ public/services/interceptor.js
@@ -1,11 +1,15 @@
 'use strict';
 
-angular.module('mean-factory-interceptor', [])
-  .factory('httpInterceptor', ['$q', '$location', '$meanConfig',
-    function($q, $location, $meanConfig) {
+angular.module('mean-factory-interceptor', ['ngCookies'])
+  .factory('httpInterceptor', ['$q', '$location', '$meanConfig', '$cookies',
+    function($q, $location, $meanConfig, $cookies) {
+
       return {
         'response': function(response) {
+
           if (response.status === 401) {
+            //console.log('response 401', response);
+            //console.log('redirecting to loginPage', $meanConfig.loginPage);
             $location.url($meanConfig.loginPage);
             return $q.reject(response);
           }
@@ -15,18 +19,85 @@ angular.module('mean-factory-interceptor', [])
         'responseError': function(rejection) {
 
           if (rejection.status === 401) {
-            $location.url($meanConfig.loginPage);
+            console.log('responseError 401', rejection);
+
+            // This is to set the cookie so that we can redirect back to the proper urL
+
+            if(rejection.config.url !== '/api/login') {
+
+              //console.log('droppin cookies for login redirect', $location.path());
+              $cookies.put('redirect', $location.path());
+              //console.log('redirecting to loginPage', $meanConfig.loginPage);
+
+            } else {
+              $location.url($meanConfig.loginPage);
+            }
+
+
             return $q.reject(rejection);
+
           }
           return $q.reject(rejection);
         }
 
       };
     }
-  ])
+  ]).factory('noCacheInterceptor', function () {
+      return {
+        request: function (config) {
+          // Don't cache GET reqs to /api/*, fix for IE
+          if(config.method=='GET' && config.url.match(/api\//)) {
+            var separator = config.url.indexOf('?') === -1 ? '?' : '&';
+            config.url = config.url+separator+'noCache=' + new Date().getTime();
+          }
+          return config;
+        }
+      };
+    }).factory('exceptionHandler', ['$injector', '$log', '$window', function($injector, $log, $window) {
+        return function($delegate) {
+          return function (exception, cause) {
+          // Lazy load to avoid circular dependency
+          var $http = $injector.get('$http');
+
+          try {
+            var errorMessage = exception.toString();
+            StackTrace.fromError(exception).then(function(stackframes) {
+              var stringifiedStack = stackframes.map(function(sf) { 
+                return sf.toString();
+              }).join(' \n');
+
+              $http.post('/api/errors/clientside', 
+              {
+                errorUrl: $window.location.href,
+                errorMessage: errorMessage,
+                stackTrace: stringifiedStack,
+                cause: ( cause || '' )
+              }).then(function(response) {
+              //...
+              }, function(err) {
+              //...
+              });
+            });
+          } catch ( loggingError ) {
+            // for Developers - log the log-failure.
+            $log.warn( "Error logging failed" );
+            $log.log( loggingError );
+          }
+
+          // Pass through to original handler
+          $delegate(exception, cause);
+
+          };
+        };
+      }])
+
   //Http Interceptor to check auth failures for XHR requests
-  .config(['$httpProvider',
-    function($httpProvider) {
+  .config(['$httpProvider', '$provide',
+    function($httpProvider, $provide) {
       $httpProvider.interceptors.push('httpInterceptor');
+      $httpProvider.interceptors.push('noCacheInterceptor');
+      $provide.decorator('$exceptionHandler', ['$delegate', 'exceptionHandlerFactory', function ($delegate, exceptionHandlerFactory) {
+        return exceptionHandlerFactory($delegate);
+      }]);
     }
   ]);
--- public/system.js
+++ public/system.js
@@ -3,6 +3,7 @@
 angular.module('mean.system')
   .run(['$rootScope', function($rootScope) {
     $rootScope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams){
+      angular.element('#navbar').removeClass('in'); // hide menu on mobile after state change
       var toPath = toState.url;
       toPath = toPath.replace(new RegExp('/', 'g'), '');
       toPath = toPath.replace(new RegExp(':', 'g'),'-');
